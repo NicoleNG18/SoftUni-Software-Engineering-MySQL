03aa37f4f1fd5f7d2a95c3403958a2c0cb6de3a9
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-08-05 10:31:31 +0300

    Data Aggregation SoftUni db exercise

--- DataAggreagation/dataAggregationSoftUniDbExercise.sql
+++ DataAggreagation/dataAggregationSoftUniDbExercise.sql
@@ -24,3 +24,47 @@ SELECT `department_id`, avg(`salary`) AS `avg_salary`
 FROM `salary_more_than`
 GROUP BY `department_id`
 ORDER BY `department_id`;
+
+USE `soft_uni1`;
+
+SELECT `department_id`, MAX(`salary`) AS `max_salary`
+FROM `employees`
+GROUP BY `department_id`
+HAVING `max_salary` < 30000
+    OR `max_salary` > 70000
+ORDER BY `department_id`;
+
+SELECT COUNT(`employee_id`) AS " "
+FROM `employees`
+WHERE `manager_id` IS NULL;
+
+SELECT `department_id`, MAX(`salary`) AS `third_highest_salary`
+FROM `employees`
+GROUP BY `department_id`
+ORDER BY `department_Id`;
+
+SELECT `department_id`, SUM(`salary`) AS `total_salary`
+FROM `employees`
+GROUP BY `department_id`
+ORDER BY `department_id`;
+
+
+SELECT DISTINCT `department_id`,
+                (SELECT DISTINCT `salary`
+                 FROM `employees` e
+                 WHERE e.`department_id` = `employees`.`department_id`
+                 ORDER BY `salary` DESC
+                 LIMIT 1 OFFSET 2) AS `third_highest_salary`
+FROM `employees`
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY `department_id`;
+
+
+
+SELECT `first_name`, `last_name`, `department_id`
+FROM `employees` f
+WHERE `salary` > (SELECT avg(`salary`)
+                  FROM `employees` e
+                  WHERE e.department_id = f.department_id)
+ORDER BY `department_id`, `employee_id`
+LIMIT 10;
