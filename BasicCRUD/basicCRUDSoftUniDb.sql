6699cb7726311549ceee0421660b24b3611f06c3
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-08-07 16:05:00 +0300

    Basic CRUD softuni db

--- BasicCRUD/basicCRUDSoftUniDb.sql
+++ BasicCRUD/basicCRUDSoftUniDb.sql
@@ -48,3 +48,91 @@ SELECT CONCAT_WS(' ',
                  `last_name`) AS 'Full Name'
 FROM `employees`
 WHERE `salary` IN (25000, 14000, 12500, 23600);
+
+
+
+SELECT `first_name`,
+       `last_name`
+FROM `employees`
+WHERE `manager_id` IS NULL;
+
+
+SELECT `first_name`, `last_name`, `salary`
+FROM `employees`
+WHERE `salary` > 50000
+ORDER BY `salary` DESC;
+
+SELECT `first_name`, `last_name`
+FROM `employees`
+ORDER BY `salary` DESC
+LIMIT 5;
+
+SELECT `first_name`,
+       `last_name`
+FROM `employees`
+WHERE NOT `department_id` = 4;
+
+SELECT *
+FROM `employees`
+ORDER BY `salary` DESC,
+         `first_name` ASC,
+         `last_name` DESC,
+         `middle_name` ASC,
+         `employee_id`;
+
+
+SELECT DISTINCT `job_title`
+FROM `employees`
+ORDER BY `job_title`;
+
+
+
+SELECT *
+FROM `projects`
+ORDER BY `start_date`, `name`
+LIMIT 10;
+
+SELECT `first_name`, `last_name`, `hire_date`
+FROM `employees`
+ORDER BY `hire_date` DESC
+LIMIT 7;
+
+UPDATE `employees`
+SET `salary` =`salary` + (0.12) * `salary`
+WHERE `department_id` IN (1, 2, 4, 11);
+
+SELECT `salary`
+FROM `employees`;
+
+USE `soft_uni`;
+
+
+CREATE VIEW `v_employees_salaries` AS
+SELECT `first_name`, `last_name`, `salary`
+FROM `employees`;
+
+SELECT *
+FROM `v_employees_salaries`;
+
+
+CREATE VIEW `v_employees_job_titles` AS
+SELECT CONCAT_WS(" ", `first_name`, `middle_name`, `last_name`) AS `full_name`, `job_title`
+FROM `employees`;
+
+SELECT *
+FROM `v_employees_job_titles`;
+
+
+
+SELECT CONCAT(`first_name`, ".", `last_name`, "@softuni.bg") AS `full_email_address`
+FROM `employees`;
+
+
+
+
+
+
+
+
+
+
