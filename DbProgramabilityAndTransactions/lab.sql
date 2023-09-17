8bf7e3d1910ca603512695decf87cd96a9622afb
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-09-17 11:10:37 +0300

    Transactions / lab

--- DbProgramabilityAndTransactions/lab.sql
+++ DbProgramabilityAndTransactions/lab.sql
@@ -31,3 +31,42 @@ end$$
 call usp_raise_salaries('Finance');
 
 $$
+
+create procedure usp_raise_salary_by_id(id int)
+begin
+    update `employees` as e
+    set `salary`=`salary` * 1.05
+    where e.`employee_id` = id;
+end $$
+
+call usp_raise_salary_by_id(17);
+$$
+
+
+
+CREATE TABLE `deleted_employees`
+(
+    `employee_id`   INT         not null auto_increment,
+    `first_name`    VARCHAR(50) not null,
+    `last_name`     VARCHAR(50) not null,
+    `middle_name`   VARCHAR(50) not null,
+    `job_title`     VARCHAR(50) not null,
+    `department_id` INT         NOT NULL,
+    `salary`        DECIMAL(19, 4),
+    primary key (`employee_id`)
+);
+$$
+
+create trigger tr_deleted_employees
+    after delete
+    on `employees`
+    for each row
+begin
+    insert into `deleted_employees` (`first_name`, `last_name`, `middle_name`, `job_title`, `department_id`, `salary`)
+    values (old.`first_name`, old.`last_name`, old.`middle_name`, old.`job_title`,
+            old.`department_id`, old.`salary`);
+end
+
+$$
+
+
