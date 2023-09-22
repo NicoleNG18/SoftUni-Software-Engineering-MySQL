04ba1c39dc78de0bf26377612e0644293bc827a8
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-09-22 14:31:08 +0300

    Transactions / exercise

--- DbProgramabilityAndTransactions/SoftUniDbExercise.sql
+++ DbProgramabilityAndTransactions/SoftUniDbExercise.sql
@@ -31,3 +31,56 @@ BEGIN
     WHERE `name` LIKE concat(letter, '%')
     ORDER BY `name`;
 END$$
+
+CREATE PROCEDURE usp_get_employees_from_town(town_name VARCHAR(50))
+BEGIN
+    SELECT `first_name`, `last_name`
+    FROM `employees` AS e
+             join `addresses` as a
+                  on e.`address_id` = a.`address_id`
+             JOIN `towns` AS t
+                  ON a.`town_id` = t.`town_id`
+    WHERE t.`name` = town_name
+    ORDER BY e.`first_name`, e.`last_name`, e.`employee_id`;
+END$$
+
+call usp_get_employees_from_town("Sofia")$$
+
+use `soft_uni`$$
+
+
+create function ufn_get_salary_level(employee_salary double)
+    returns varchar(8)
+    deterministic
+begin
+    declare salary_Level varchar(8);
+    if employee_salary < 30000 then
+        set salary_Level := "Low";
+    elseif employee_salary between 30000 and 50000 then
+        set salary_Level := "Average";
+    else
+        set salary_Level := "High";
+    end if;
+    return salary_Level;
+end$$
+
+
+
+create procedure usp_get_employees_by_salary_level(salary_level varchar(8))
+begin
+    select `first_name`, `last_name`
+    from `employees`
+    where ufn_get_salary_level(`salary`) like salary_level
+    order by `first_name` desc, `last_name` desc;
+end$$
+
+
+call usp_get_employees_by_salary_level("High");
+
+
+create function ufn_is_word_comprised(set_of_letters varchar(50), word varchar(50))
+    returns int
+    deterministic
+begin
+    RETURN word REGEXP (CONCAT('^[', set_of_letters, ']+$'));
+end$$
