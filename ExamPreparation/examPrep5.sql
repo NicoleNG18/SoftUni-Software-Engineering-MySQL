25e53195ebc35f56ca651f74c3d08b2632126189
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-09-30 09:26:57 +0300

    Exam Preparation 5

--- ExamPreparation/examPrep5.sql
+++ ExamPreparation/examPrep5.sql
@@ -15,3 +15,31 @@ begin
 end$$
 
 SELECT udf_stadium_players_count('Jaxworks') as `count`$$
+
+create procedure udp_find_playmaker(min_dribble_points int, team_name varchar(45))
+begin
+    select concat(p.`first_name`, " ", p.`last_name`) as `full_name`,
+           p.`age`,
+           p.`salary`,
+           sd.`dribbling`,
+           sd.`speed`,
+           t.`name`
+    from `players` as p
+             join `skills_data` as sd
+                  on sd.`id` = p.`skills_data_id`
+             join `teams` as t
+                  on p.`team_id` = t.`id`
+    where t.`name` = team_name
+      and sd.`dribbling` >
+          min_dribble_points
+      and sd.`speed` > (select avg(`speed`) from `skills_data`)
+    order by sd.`speed` desc
+    limit 1;
+end$$
+
+
+CALL udp_find_playmaker(20, 'Skyble')$$
+
+
+
+
