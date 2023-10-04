ea67be123355532c62303267cf2f2f1551dea0fc
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-10-04 19:57:21 +0300

    Exam Preparation 7

--- ExamPreparation/examPrep7.3.sql
+++ ExamPreparation/examPrep7.3.sql
@@ -12,3 +12,23 @@ FROM `waiters` AS w
          LEFT JOIN
      `orders` AS o ON o.`waiter_id` = w.`id`
 WHERE o.`waiter_id` IS NULL;
+
+select o.`table_id`,
+       t.`capacity`,
+       count(c.`id`) as `count_clients`,
+       (
+           case
+               when t.`capacity` > count(o.`table_id`) then "Free seats"
+               when t.`capacity` = count(o.`table_id`) then "Full"
+               when t.`capacity` < count(o.`table_id`) then "Extra seats"
+               end)  as `availability`
+from `orders` as o
+         join `tables` as t
+              on o.`table_id` = t.`id`
+         join `orders_clients` as oc
+              on oc.`order_id` = o.`id`
+         join `clients` as c
+              on c.`id` = oc.`client_id`
+where t.`floor` = 1
+group by o.`table_id`
+order by o.`table_id` desc;
