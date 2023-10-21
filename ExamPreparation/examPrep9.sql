e8862512dfa7a6a8d3c60c2f499ae5d3b2c431aa
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-10-21 09:40:43 +0300

    Exam Preparation 9

--- ExamPreparation/examPrep9.sql
+++ ExamPreparation/examPrep9.sql
@@ -26,3 +26,48 @@ CREATE TABLE `reviews`
     `picture_url`  VARCHAR(80)    NOT NULL,
     `published_at` DATETIME       NOT NULL
 );
+
+CREATE TABLE `brands`
+(
+    `id`   INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(40) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE `categories`
+(
+    `id`   INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(40) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE `products`
+(
+    `id`                INT AUTO_INCREMENT PRIMARY KEY,
+    `name`              VARCHAR(40)    NOT NULL,
+    `price`             DECIMAL(19, 2) NOT NULL,
+    `quantity_in_stock` INT,
+    `description`       TEXT,
+    `brand_id`          INT            NOT NULL,
+    `category_id`       INT            NOT NULL,
+    `review_id`         INT,
+    CONSTRAINT fk_brands_products FOREIGN KEY (`brand_id`)
+        REFERENCES `brands` (`id`),
+    CONSTRAINT fk_categories_products FOREIGN KEY (`category_id`)
+        REFERENCES `categories` (`id`),
+    CONSTRAINT fk_reviews_products FOREIGN KEY (`review_id`)
+        REFERENCES `reviews` (`id`)
+);
+
+
+CREATE TABLE `orders_products`
+(
+    `order_id`   INT,
+    `product_id` INT,
+    KEY (`order_id`, `product_id`),
+    CONSTRAINT fk_op_o FOREIGN KEY (`order_id`)
+        REFERENCES `orders` (`id`),
+    CONSTRAINT fk_op_p FOREIGN KEY (`product_id`)
+        REFERENCES `products` (`id`)
+);
+
